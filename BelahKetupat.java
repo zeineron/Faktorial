a511211fc58cdbe75bcfd18095e75936c7954448
zeineron <zeineron@gmail.com>
2017-04-07 10:25:42 +0700

    Add files via upload
    Update Belah Ketupat

--- BelahKetupat.java
+++ BelahKetupat.java
@@ -22,23 +22,28 @@ public class BelahKetupat {
         int n = 12 / 2;
         for (int i = 1; i <= n; i++) {
             for (int k = n; k > i; k--) {
-                System.out.print(" ");
+                System.out.print("0");
             }
             for (int j = 1; j <= l; j++) {
                 System.out.print("+");
             }
+            for (int k = i; k < tingkat-n+2; k++) {
+                System.out.print("0");
+            }
             l += 2;
-
             System.out.println("");
         }
         l -= 4;
 
         for (int i = (n - 1); i >= 1; i--) {
             for (int k = i; k <= (n - 1); k++) {
-                System.out.print(" ");
+                System.out.print("0");
             }
             for (int j = l; j >= 1; j--) {
                 System.out.print("+");
+            }
+            for (int k = i; k < tingkat-n+2; k++) {
+                System.out.print("0");
             }
             l -= 2;
 
